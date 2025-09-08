270a7cb96193c2451be69f2f7ee4f070b2682fae
Laurence Hupp <laurence.hupp@airbus.com>
2025-09-08 10:49:33 +0200

    init

---
+++ .config/nvim/lua/laur/init.lua
@@ -0,0 +1,23 @@
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+require 'laur.options'
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+require 'laur.lazy_init'
+require 'laur.keymap'
+-- vim: ts=2 sts=2 sw=2 et
